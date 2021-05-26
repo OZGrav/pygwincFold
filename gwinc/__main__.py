d21db3b118f76bb1ff3bff8f7c9e063d5f4fbabe
Jameson Graef Rollins <jameson.rollins@ligo.org>
2021-05-26 15:16:29 -0700

    cli: fix bug when calculating range

--- gwinc/__main__.py
+++ gwinc/__main__.py
@@ -148,6 +148,7 @@ def main():
             parser.exit(2, f"Error: {e}\n")
         name = budget.name
         ifo = budget.ifo
+        freq = budget.freq
         plot_style = getattr(budget, 'plot_style', {})
         trace = None
 
