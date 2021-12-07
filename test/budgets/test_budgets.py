2c88b04cf2f2665bca9d4397ec0ad63692a56976
Kevin Kuns <kevin.kuns@ligo.org>
2021-12-07 17:37:35 -0500

    fix bug when updating ifo struct in budget run

--- test/budgets/test_budgets.py
+++ test/budgets/test_budgets.py
@@ -1,7 +1,10 @@
 """
 """
+import numpy as np
 import gwinc
 from gwinc import load_budget
+from copy import deepcopy
+import pytest
 
 
 def test_load(pprint, tpath_join, fpath_join):
@@ -11,4 +14,36 @@ def test_load(pprint, tpath_join, fpath_join):
         trace = B.run()
         fig = trace.plot()
         fig.savefig(tpath_join('budget_{}.pdf'.format(ifo)))
+
+
+@pytest.mark.logic
+@pytest.mark.fast
+def test_update_ifo_struct():
+    """
+    Test that the noise is recalculated when the ifo struct is updated
+    """
+    budget = gwinc.load_budget('CE2silica')
+    tr1 = budget.run()
+    budget.ifo.Suspension.VHCoupling.theta *= 2
+    tr2 = budget.run()
+    assert np.all(
+        tr2.Seismic.SeismicVertical.asd == 2*tr1.Seismic.SeismicVertical.asd)
+
+
+@pytest.mark.logic
+@pytest.mark.fast
+def test_change_ifo_struct():
+    """
+    Test that the noise is recalculated when a new ifo struct is passed to run
+    """
+    budget = gwinc.load_budget('CE2silica')
+    ifo1 = deepcopy(budget.ifo)
+    ifo2 = deepcopy(budget.ifo)
+    ifo2.Suspension.VHCoupling.theta *= 2
+    tr1 = budget.run(ifo=ifo1)
+    tr2 = budget.run(ifo=ifo2)
+    tr3 = budget.run(ifo=ifo1)
+    assert np.all(tr1.asd == tr3.asd)
+    assert np.all(
+        tr2.Seismic.SeismicVertical.asd == 2*tr1.Seismic.SeismicVertical.asd)
 
